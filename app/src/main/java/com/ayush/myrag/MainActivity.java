f08cca80714ae475c8f3066c8f95c6e4643fe81e
Ayush <mangoreayush@gmail.com>
2021-06-27 18:49:06 +0530

    Comments Added

--- app/src/main/java/com/ayush/myrag/MainActivity.java
+++ app/src/main/java/com/ayush/myrag/MainActivity.java
@@ -21,6 +21,14 @@ import com.karumi.dexter.listener.single.PermissionListener;
 import java.io.File;
 import java.util.ArrayList;
 
+/*
+* In this project I have created a song player which reads songs from the storage of the phone and show them all
+* in a simple list view, where user can select any song from the list and then user can do various things in it like
+* user can play or pause the song user can choose next/previous song and activity is provided with impressive UI.
+* I have used Dexter library in this application
+*  Dexter is an android library that simplifies the process of requesting permissions at runtime.
+* we can ask the user while running the application weather user want to deny or allow for the permissions required.
+* */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -28,21 +36,42 @@ public class MainActivity extends AppCompatActivity {
         ListView listView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         listView = findViewById(R.id.listview);
+
+        //Asking user to allow the application to read storage
         Dexter.withContext(this)
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
                     @Override
+                    /* if user grants the permission then we will read all mp3 files available in the device storage
+                    *  for this we will use File class
+                    * Java File class represents the files and directories path names in an abstract manner.
+                    * This class is used for creation of files and directories, file searching, file deletion etc.
+                    * The File object represents the actual file/directory on the disk.
+                    * */
                     public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
 //                        Toast.makeText(MainActivity.this,"Runtime Permission Given",Toast.LENGTH_SHORT).show();
+                        /* now we will collect all the mp3 files in mysongs named array list with the help of fetchsongs method
+                           and then in a string array we will stroe the names of all the songs to populate the list
+                        * */
                         ArrayList<File> mySongs = fetchSongs(Environment.getExternalStorageDirectory());
                         String[] items = new String[mySongs.size()];
                         for(int i=0;i<mySongs.size();i++){
                             items[i] = mySongs.get(i).getName().replace(".mp3","");
                         }
-
+                        /*
+                        * After extracting names of all the songs we will used default adapter and populate the
+                        * list with the names of the songs, with the help of which user can choose any song*/
                         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,items);
                         listView.setAdapter(adapter);
+
+                        /*
+                        after populating, if user click in any of the song user will be taken to new activity with the
+                        help of intent and we will also provide some extra information along with the desired song like
+                        list of songs, and it's position.
+                        Now we will start the new activity
+                         */
                         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                             @Override
                             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -69,15 +98,22 @@ public class MainActivity extends AppCompatActivity {
                 .check();
     }
 
+    /*
+    * fetchsongs() method is the backbone of this whole application , as it takes the whole file directory as input
+    * and returns the list of only valid .mp3 extension files.
+     */
     public ArrayList<File> fetchSongs(File file){
         ArrayList arrayList = new ArrayList();
+        //getting the flist of files/directory in songs array
         File[] songs = file.listFiles();
         if(songs != null){
             for(File myfile : songs){
+                // if any file in songs array is a directory then we will recursively call method again to read files inside the directory
                 if(myfile.isDirectory() && (!myfile.isHidden())){
                     arrayList.addAll(fetchSongs(myfile));
                 }
                 else {
+                    // only accept those files that have extension .mp3
                     if(myfile.getName().endsWith(".mp3") && !myfile.getName().startsWith(".")){
                         arrayList.add(myfile);
                     }
--- app/src/main/java/com/ayush/myrag/PlaySong.java
+++ app/src/main/java/com/ayush/myrag/PlaySong.java
@@ -14,8 +14,16 @@ import android.widget.TextView;
 import java.io.File;
 import java.util.ArrayList;
 
+/*
+ * This class is basically used to provide all the functionalities for the user like play,pause,next,previous etc.
+ * also this class have methods to monitor seek bar progress as well which will display the progress of the song.
+ */
 public class PlaySong extends AppCompatActivity {
     @Override
+    /*
+    if user press back button then the activity will destroy , while closing we will stop the media player
+    and release it so that it can be assigned to another media file.
+     */
     protected void onDestroy() {
         super.onDestroy();
         mediaPlayer.stop();
@@ -31,6 +39,7 @@ public class PlaySong extends AppCompatActivity {
     String textContent;
     int position;
     Thread updateSeek;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,16 +50,27 @@ public class PlaySong extends AppCompatActivity {
         next = findViewById(R.id.next);
         seekBar = findViewById(R.id.seekBar);
 
+        /*
+        we will get all the extra information that has been provided along with intents from Mainactivity class in bundle.
+         */
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
+        // Extracting the list of all songs from the bundle
         songs = (ArrayList) bundle.getParcelableArrayList("songList");
+        // setting the text as the name of the current song(choosen by user)
         textContent = intent.getStringExtra("currentSong");
         textView.setText(textContent);
         textView.setSelected(true);
         position = intent.getIntExtra("position",0);
+        /*
+        Uri stands for uniform resource identifier.
+        A uniform resource identifier is a sequence of characters used for identification of a particular resource.
+         */
         Uri uri = Uri.parse(songs.get(position).toString());
+        //Then will start to play the media file
         mediaPlayer = MediaPlayer.create(this,uri);
         mediaPlayer.start();
+        //will set the seekbar max limit to the duration of the current song
         seekBar.setMax(mediaPlayer.getDuration());
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -71,7 +91,10 @@ public class PlaySong extends AppCompatActivity {
         });
 
 
-
+        /*
+        If play button is pressed then we will change the image as well as pause the song if song is currently playing.
+        if song is pause currently then we will change the image and start the song.
+         */
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -87,6 +110,10 @@ public class PlaySong extends AppCompatActivity {
             }
         });
 
+     /*
+     * If previous button is pressed then we will pause the current song and play the previous song in the list.
+     * In circular manner
+      * */
         previous.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -99,6 +126,7 @@ public class PlaySong extends AppCompatActivity {
                 else{
                     position = songs.size()-1;
                 }
+                // playing the previous song
                 Uri uri = Uri.parse(songs.get(position).toString());
                 mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);
                 mediaPlayer.start();
@@ -109,6 +137,10 @@ public class PlaySong extends AppCompatActivity {
             }
         });
 
+        /*
+         * If next button is pressed then we will pause the current song and play the next song in the list.
+         * In circular manner
+         */
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -121,6 +153,7 @@ public class PlaySong extends AppCompatActivity {
                 else{
                     position = 0;
                 }
+                // playing the next song
                 Uri uri = Uri.parse(songs.get(position).toString());
                 mediaPlayer = MediaPlayer.create(getApplicationContext(),uri);
                 mediaPlayer.start();
@@ -130,6 +163,9 @@ public class PlaySong extends AppCompatActivity {
 
             }
         });
+        /*
+        with the help of thread we will update the seekbar in every 500 milliseconds.
+         */
         updateSeek = new Thread(){
             @Override
             public void run() {
